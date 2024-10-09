f19cea6f3284b9827a61e7f05bd32e1dc40e0f6e
Konrad Malik <konrad.malik@gmail.com>
2024-10-09 21:58:09 +0200

    chore: add nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "git-conflict.nvim";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [ pkgs.hello ];
+          };
+          formatter = pkgs.nixfmt-rfc-style;
+        };
+    };
+}
